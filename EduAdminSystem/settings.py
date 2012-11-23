4a64ebc68fe77e1d8c9d47d3c72ca1c224749890
Zonyitoo <zonyitoo@gmail.com>
2012-11-23 20:59:06 +0800

    modify static root, do collectstatic everytime you modify js/css/img files

--- EduAdminSystem/settings.py
+++ EduAdminSystem/settings.py
@@ -59,7 +59,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static/')
+#STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static/')
+STATIC_ROOT = 'static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -69,8 +70,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    'static', 
+    # Don't forget to use absolute paths, not relative paths. 
     'user/static/', 
     'assessment/static/', 
     'course/static/',
@@ -78,6 +78,7 @@ STATICFILES_DIRS = (
     'take/static/',
     'student/static/', 
     'teacher/static/',
+    'EduAdminSystem/static/', 
 )
 
 # List of finder classes that know how to find static files in
--- static/js/login.js
+++ static/js/login.js
@@ -1,4 +1,51 @@
 // JavaScript Document
+$(document).ready(function(){ 
+
+  var csrftoken = getCookie('csrftoken');
+//  alert("CSRF Token: " + csrftoken);
+
+  $.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+      if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
+        xhr.setRequestHeader("X-CSRFToken", csrftoken);
+      }
+    }
+  });
+}); 
+
+function getCookie(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie != '') {
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i ++) {
+      var cookie = jQuery.trim(cookies[i]);
+      if (cookie.substring(0, name.length + 1) == (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
+
+function csrfSafeMethod(method) {
+  // These HTTP methods do not require CSRF protection
+  return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+function sameOrigin(url) {
+  // test that given url is a same-origin url
+  // url could be relative or scheme relative or absolute
+  var host = document.location.host;
+  var protocol = document.location.protocol;
+  var sr_origin = '//' + host;
+  var origin = protocol + sr_origin;
+  // Allow absolute or scheme relative URLs to same origin
+       return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
+               (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+              // or any other URL that isn't scheme relative or absolute i.e relative.
+              !(/^(\/\/|http:|https:).*/.test(url));
+}
 
 function validate()
 {
@@ -18,6 +65,7 @@ function validate()
             url: "/user/login/", //访问路径
             data: "username=" + $("#username").val() + "&passwd=" + $("#passwd").val(), //需要验证的参数
             type: "post", //传值的方式
+            async: false,
             error: function ()
             {//访问失败时调用的函数
                 alert("链接服务器错误！");
