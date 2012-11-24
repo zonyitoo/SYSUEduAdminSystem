337b0547b45eef769e67bbfa8076b6b71c21e6b2
Zonyitoo <zonyitoo@gmail.com>
2012-11-24 22:19:52 +0800

    Better createTestData

--- createTestData.py
+++ createTestData.py
@@ -4,31 +4,9 @@ from EduAdminSystem import settings
 from django.core.management import setup_environ
 setup_environ(settings)
 from django.contrib.auth.models import User
-from school.models import *
-from student.models import Student, StudentMeta
-from teacher.models import Teacher
 
-students = [
-        {
-            'username': '10383001',
-            'password': '123456',
-            'student_name': 'ABC'
-        },
-        {
-            'username': '10383002',
-            'password': '123456',
-            'student_name': 'BCD'
-        }
-    ]
-
-teachers = [
-        {
-            'username': 'jqg',
-            'password': 'jqg',
-            'teacher_name': '纪庆革',
-            'title': 'P'
-        }    
-    ]
+## Schools
+from school.models import School, Department, Speciality
 
 sist = School.objects.get_or_create(name='SIST')[0]
 sist.save()
@@ -37,25 +15,74 @@ cs.save()
 cst = Speciality.objects.get_or_create(name='CST', department=cs)[0]
 cst.save()
 
+# Students
+from student.models import Student, StudentMeta
+
 stuMeta = StudentMeta.objects.get_or_create(type_name='UG', year='2010',
         major=cst)[0]
 
+students = [
+        {
+            'user': {
+                    'username': '10383001',
+                    'password': '123456'
+                },
+            'student': {
+                'student_name': 'ABC',
+                'student_meta': StudentMeta.objects.get(year=2010)
+                }
+            },
+        {
+            'user': {
+                'username': '10383002',
+                'password': '123456',
+                },
+            'student': {
+                'student_name': 'BCD',
+                'student_meta': StudentMeta.objects.get(year=2010)
+                }
+            },
+        {
+            'user': {
+                'username': '10383067',
+                'password': 'zonyitoo',
+                },
+            'student': {
+                'student_name': '钟宇腾',
+                'student_meta': StudentMeta.objects.get(year=2010)
+                }
+            }
+        ]
+
 for student in students:
-    user = User.objects.get_or_create(username=student['username'])[0]
-    user.set_password(student['password'])
+    user = User.objects.get_or_create(**student['user'])[0]
     user.save()
-    stud = Student.objects.get_or_create(student_name=student['student_name'],
-            student_meta=stuMeta, user=user)[0]
+    stud = Student.objects.get_or_create(user=user, **student['student'])[0]
     stud.save()
 
+## Teachers
+from teacher.models import Teacher
+teachers = [
+        {
+            'user': {
+                'username': 'jqg',
+                'password': 'jqg',
+                },
+            'teacher': {
+                'teacher_name': '纪庆革',
+                'title': Teacher.TITLE[2][0],
+                'department': Department.objects.get(name='CS')
+                }
+            }    
+        ]
+
 for teacher in teachers:
-    user = User.objects.get_or_create(username=teacher['username'])[0]
-    user.set_password(teacher['password'])
+    user = User.objects.get_or_create(**teacher['user'])[0]
     user.save()
-    teac = Teacher.objects.get_or_create(teacher_name=teacher['teacher_name'],
-            department=cs, user=user, title=teacher['title'])[0]
+    teac = Teacher.objects.get_or_create(user=user, **teacher['teacher'])[0]
     teac.save()
     
+## Courses
 from course.models import CourseType, Course
 for t in CourseType.COURSE_TYPE:
     CourseType.objects.get_or_create(type_name=t[0])[0].save()
@@ -76,7 +103,7 @@ courses = [
             'course_type':
                 CourseType.objects.get(type_name=CourseType.COURSE_TYPE[0][0]),
             'department': cs
-        } 
+            } 
     ]
 
 for c in courses:
