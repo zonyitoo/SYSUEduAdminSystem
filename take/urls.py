d98b09c117a041b6ca9f5354e4362c381313c8fb
iphkwan <yexjun@gmail.com>
2012-11-25 11:15:59 +0800

    增加“查看教学计划”的方法：getTakePlan，天冷，拿衣服，缺debug

--- take/urls.py
+++ take/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('', 
         url(r'getTakeCourses/$', 
-                'take.views.get_take_courses'), 
+                'take.views.get_take_courses'),
+        url(r'getTakePlan/$',
+                'take.views.get_take_plan'),
 )
--- take/views.py
+++ take/views.py
@@ -44,4 +44,47 @@ def get_take_courses(request):
                 mimetype='application/json')
     else:
         return HttpResponseBadRequest('Invalid Method')
+
+@login_required
+def get_take_plan(request):
+    if request.method == 'GET':
+        cultivate = int(request.GET.get('cultivate', 0))
+        
+        # only work for student 
+        user = request.user
+        student = Student.objects.get(user = user)
+        
+        # only work for major
+        department = None
+        if cultivate == 0:
+            department = student.student_meta.major.department
+
+        take = Takes.objects.filter(student__user__exact = user,
+                course__department__exact = department)
             
+        courseArr = []
+        for take in takes:
+            courseObj = {}
+            courseObj['name'] = take.course.name
+            courseObj['academic_year'] = take.course.academic_year
+            courseObj['semester'] = take.course.semester
+            courseObj['from_week'] = take.course.from_week
+            courseObj['to_week'] = take.course.to_week
+            courseObj['course_time'] = take.course.course_time
+            courseObj['teacher'] = take.course.teacher.teacher_name
+            courseObj['credit'] = take.course.credit
+            courseObj['location'] = take.course.location
+            courseObj['capacity'] = take.course.capacity
+            courseObj['exam_method'] = take.course.exam_method
+            courseObj['course_type'] = CourseType.get_coursetype(take.course.course_type)
+            courseObj['department'] = take.course.department.name
+            
+            courseArr.append(courseObj)
+
+        retJson = {'courses': courseArr, 
+                'studentid': request.user.username
+                }
+        return HttpResponse(simplejson.dumps(retJson),
+                mimetype='application/json')
+    else:
+        return HttpResponseBadRequest('Invalid Method')
