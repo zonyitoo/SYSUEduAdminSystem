62c9d59bf4a3a3b18d9a415851fbd78c6213f9bc
Zonyitoo <zonyitoo@gmail.com>
2012-11-26 20:44:10 +0800

    Modified student select/withdrawal courses

--- student/urls.py
+++ student/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('', 
-    url('selectCourse/$', 'student.views.select_course'),
-    url('withdrawalCourse/$', 'student.views.withdrawal_course'),
+    #url('selectCourse/$', 'student.views.select_course'),
+    #url('withdrawalCourse/$', 'student.views.withdrawal_course'),
+    url('toggleCourse/$', 'student.views.toggle_course'),
 )
--- student/views.py
+++ student/views.py
@@ -1,28 +1,15 @@
 # -*- coding:utf-8 -*-
 
-from django.http import HttpResponseForbidden
+from django.http import HttpResponseForbidden, HttpResponseBadRequest
 from take.models import Takes
 from course.models import Course
 from models import Student
 from ajaxutils.decorators import ajax
 
-@ajax(login_required=True, require_POST=True)
 def select_course(request):
     pass
 
-@ajax(login_required=True, require_POST=True)
-def withdrawal_course(request):
-    if not hasattr(request.user, 'student'):
-        return HttpResponseForbidden('Only Student can withdrawal')
-
-    course_id = request.POST['course_id']
-    course = None
-    try:
-        course = Course.objects.get(id=int(course_id))
-    except:
-        return HttpResponseForbidden('该课程不存在')
-
-    student = Student.objects.get(user=request.user)
+def withdrawal_course(student, course):
     try:
         take = Takes.objects.get(course=course, student=student)
         take.delete()
@@ -32,3 +19,17 @@ def withdrawal_course(request):
     return {'valid': True, 
         'hastaken': Takes.objects.filter(course=course, student=student).count()}
     
+@ajax(login_required=True, require_POST=True)
+def toggle_course(request):
+    student = Student.object.get(user=request.user)
+    try:
+        course = Course.objects.get(id=int(request.POST['course_id']))
+    except Course.DoesNotExist:
+        return HttpResponseForbidden('该课程不存在')
+
+    if request.POST['state'] == '1':
+        select_course(student, course)
+    elif request.POST['state'] == '0':
+        withdrawal_course(student, course)
+    else:
+        return HttpResponseBadRequest('Invalid Command')
