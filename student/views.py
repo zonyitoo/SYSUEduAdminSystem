823e4305b192e24c0e4ce5965cf6df27c01442c7
sheepke <sheepke@126.com>
2012-11-26 20:03:23 +0800

    modified:   views.py

--- student/views.py
+++ student/views.py
@@ -9,7 +9,31 @@ from models import Student
 
 @login_required
 def select_course(request):
-    pass
+    if request.method == 'POST':
+        if not hasattr(request.user, 'student'):
+            return HttpResponseBadRequest('Only Student can select course')
+
+        course_id = request.POST['course_id']
+        course = None
+        try:
+            course = Course.objects.get(id=int(course_id))
+        except:
+            return HttpResponse(simplejson.dumps({'valid':False,  'error_msg':
+                '该课程不存在'}), mimetype='application/json')
+
+        student = Student.objects.get(user=request.user)
+        try:
+            take = Takes.objects.create(course=course,student=student)
+            take.save()
+        except:
+            pass
+
+        return HttpResponse(simplejson.dumps({'valid': True,
+            'hastaken':Takes.objects.filter(course=course,student=student).count()}),
+            mimetype='application/json')
+
+    else:
+        return HttpResponseBadRequest('Invalid Method')
 
 @login_required
 def withdrawal_course(request):
