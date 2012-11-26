f7645f8b018112793001bb2489af273c27dfc34f
19thhell <yangxh92@gmail.com>
2012-11-27 00:58:41 +0800

    auto generate school-year(until this year) and auto select

--- EduAdminSystem/static/js/getperiod.js
+++ EduAdminSystem/static/js/getperiod.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
     var year = date.getFullYear();
     var month = date.getMonth() + 1;
     var term = 0;
+    var i = 2005;
     if (month < 9)
         year--;
     if (month >= 9 || month < 2)
@@ -12,6 +13,8 @@ $(document).ready(function() {
     else if (month >= 2 && month < 7)
         term = 2;
     else term = 3;
+    for (i = i;i <= year;i++)
+        $(".school-year").append("<option class='" + i + "'>" + i + "-" + (i + 1) + "</option>");
     $(".school-year option." + year).attr("selected","selected");
     $(".school-term option." + term).attr("selected","selected");
 });
