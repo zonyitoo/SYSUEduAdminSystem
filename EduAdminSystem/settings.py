ed6d12667762ffc59f7e7dfcf55972b33781308d
Zonyitoo <zonyitoo@gmail.com>
2012-11-27 21:45:36 +0800

    Heroku Postgresql db

--- EduAdminSystem/settings.py
+++ EduAdminSystem/settings.py
@@ -192,5 +192,5 @@ LOGGING = {
 }
 
 # Parse database configuration from $DATABASE_URL
-#import dj_database_url
-#DATABASES['default'] = dj_database_url.config()
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
