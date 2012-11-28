20e4a0e8c733a886235b8934bd35144a328a308b
iphkwan <yexjun@gmail.com>
2012-11-28 22:37:46 +0800

    modified:   EduAdminSystem/settings.py

--- EduAdminSystem/settings.py
+++ EduAdminSystem/settings.py
@@ -17,7 +17,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'easdb',                      # Or path to database file if using sqlite3.
         'USER': 'eas',                      # Not used with sqlite3.
-        'PASSWORD': '123',                  # Not used with sqlite3.
+        'PASSWORD': 'eduadminsystem',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
